470245875e25d00724401c92850fee9f67075e1c
권용빈 <yiyb0603@gmail.com>
2024-01-06 17:11:40 +0900

    feature: zod를 사용한 환경변수 검증 추가

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
+import { ZodError } from 'zod';
+import { dotenv, dotenvSchema } from './libs/dotenv';
 import { app } from './app';
-import { dotenv } from './libs/dotenv';
 
-const port = dotenv.SERVER_PORT || 3000;
+dotenvSchema
+  .parseAsync(dotenv)
+  .then(() => {
+    const port = dotenv.SERVER_PORT || 3000;
 
-app.listen(port, () =>
-  console.log(`🚀 Server ready at: http://localhost:${port}`),
-);
+    app.listen(port, () =>
+      console.log(`🚀 Server ready at: http://localhost:${port}`),
+    );
+  })
+  .catch((error) => {
+    const zodError = error as ZodError;
+
+    if (zodError.issues.length > 0) {
+      console.log(`환경변수 오류: ${zodError.issues[0].message}`);
+    } else {
+      console.log('오류 발생');
+    }
+
+    process.exit(1);
+  });
--- src/libs/dotenv.ts
+++ src/libs/dotenv.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
+import { z } from 'zod';
 
 config();
 
@@ -12,3 +13,31 @@ export const dotenv = {
 
   JWT_SECRET: process.env.JWT_SECRET as string,
 } as const;
+
+export const dotenvSchema = z
+  .object({
+    DATABASE_URL: z.string().min(1, '데이터베이스 경로를 입력해주세요.'),
+
+    SERVER_PORT: z.number().int().min(1, '서버 포트번호를 입력해주세요'),
+
+    ACCESS_TOKEN_EXPIRES: z
+      .number()
+      .int()
+      .min(1, 'Access Token 만료시간(초)를 입력해주세요.'),
+    REFRESH_TOKEN_EXPIRES: z
+      .number()
+      .min(1, 'Refresh Token 만료시간(초)를 입력해주세요.'),
+
+    JWT_SECRET: z.string().min(1, 'JWT SECRETS 값을 입력해주세요.'),
+  })
+  .superRefine(
+    ({ ACCESS_TOKEN_EXPIRES, REFRESH_TOKEN_EXPIRES }, { addIssue }) => {
+      if (ACCESS_TOKEN_EXPIRES >= REFRESH_TOKEN_EXPIRES) {
+        addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            'Access Token 만료시간이 Refresh Token 만료시간보다 크거나 같을 수 없습니다.',
+        });
+      }
+    },
+  );
